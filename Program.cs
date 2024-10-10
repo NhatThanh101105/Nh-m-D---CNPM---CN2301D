36d81e9a873c729358c94043fb35ff14caaa2fe1
QuocHoangFOM <168975831+QuocHoangFOM@users.noreply.github.com>
2024-10-10 15:01:16 +0700

    Upload basic

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApplication_GameCaKoi // Sử dụng không gian tên cho Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
