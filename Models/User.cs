be0b75781898f512431312c1307c4545c8453c71
QuocHoangFOM <168975831+QuocHoangFOM@users.noreply.github.com>
2024-10-30 13:49:24 +0700

    Delete Models directory

--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebApplication_GameCaKoi.Models // Sử dụng không gian tên cho mô hình
-{
-    public class User : IdentityUser
-    {
-        public decimal Balance { get; set; }
-    }
-}
