1d2f179e78f0d1573258f3c4496a1bb9cdb1fbc9
NhatThanh101105 <nhatthanh10112005@gmail.com>
2024-10-30 20:46:54 +0700

    Add files via upload
    ae tham khảo thử nhen cái html với css

---
+++ Pages/Shared/StorePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiFish.Pages.Shared
+{
+    public class StorePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
