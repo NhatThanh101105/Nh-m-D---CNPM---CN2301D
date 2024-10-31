ea0207d080b89887a33210209bdf971d4184ec6f
QuocHoangFOM <hoangquocfw@gmail.com>
2024-10-31 22:42:17 +0700

    nap the

---
+++ Koi_Game_Reposities/Interfaces/IKoiRepository.cs
@@ -0,0 +1,16 @@
+﻿using Koi_Game_Reposities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koi_Game_Reposities.Interfaces
+{
+    public  interface IKoiRepository
+    {
+        Task<List<KoiFish>> GetAllKoiFishs();
+        Task<KoiFish> GetKoiFishById(int id);
+
+    }
+}
