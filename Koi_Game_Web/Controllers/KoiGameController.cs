87c5ab28ef644c050110760b7827b14eaaaf1d6c
QuocHoangFOM <hoangquocfw@gmail.com>
2024-11-01 11:59:05 +0700

    Them trang gioi thieu va Controller

---
+++ Koi_Game_Web/Controllers/KoiGameController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KoiBreedingGame.Controllers
+{
+    public class KoiGameController : Controller
+    {
+        // GET: KoiGame/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
